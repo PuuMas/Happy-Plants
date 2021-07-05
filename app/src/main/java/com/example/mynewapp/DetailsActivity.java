20c25191aae06727547d457ac8ba57e97875c249
PuuMas <PuuMas@users.noreply.github.com>
2021-07-06 01:36:37 +0300

    Added detail info on the selected plant.

--- app/src/main/java/com/example/mynewapp/DetailsActivity.java
+++ app/src/main/java/com/example/mynewapp/DetailsActivity.java
@@ -16,12 +16,12 @@ public class DetailsActivity extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.image_details);
         TextView name = findViewById(R.id.tv_detail_name);
-        TextView info = findViewById(R.id.info);
+        TextView latin_name_txt = findViewById(R.id.textView2);
         TextView temp = findViewById(R.id.tempature_set);
-        TextView water = findViewById(R.id.water_set);
-        TextView humidity = findViewById(R.id.hum_set);
-        TextView sun = findViewById(R.id.sun_set);
-        TextView ph = findViewById(R.id.ph_set);
+        TextView water_txt = findViewById(R.id.water_set);
+        TextView humidity_txt = findViewById(R.id.hum_set);
+        TextView sun_txt = findViewById(R.id.sun_set);
+        TextView ph_txt = findViewById(R.id.ph_set);
         TextView your_temp = findViewById(R.id.your_temp_set);
         TextView your_water = findViewById(R.id.your_water_set);
         TextView your_humidity = findViewById(R.id.your_hum_set);
@@ -29,13 +29,33 @@ public class DetailsActivity extends AppCompatActivity {
         TextView your_ph = findViewById(R.id.your_ph_set);
 
         String plant_name = "Plant name not set";
+        String latin_name = "Plant latin name not set";
+        int tempInt = 0;
+        String water = "Water level not set";
+        float humidity = 0;
+        String sun = "Sun level not set";
+        float ph = 0;
+
 
         Bundle extras = getIntent().getExtras();
         if(extras != null){
             plant_name = extras.getString("herb_name");
+            latin_name = extras.getString("latin_name");
+            tempInt = extras.getInt("temp");
+            water = extras.getString("water");
+            humidity = extras.getFloat("humidity");
+            sun = extras.getString("sun");
+            ph = extras.getFloat("ph");
+
         }
 
         name.setText(plant_name);
+        latin_name_txt.setText(latin_name);
+        water_txt.setText(water);
+        sun_txt.setText(sun);
+        temp.setText(String.valueOf(tempInt));
+        humidity_txt.setText(String.valueOf(humidity));
+        ph_txt.setText(String.valueOf(ph));
 
     }
 }
--- app/src/main/java/com/example/mynewapp/MainActivity.java
+++ app/src/main/java/com/example/mynewapp/MainActivity.java
@@ -72,6 +72,12 @@ public class MainActivity extends AppCompatActivity{
         listener = (v, position) -> {
             Intent intent = new Intent(getApplicationContext(), DetailsActivity.class);
             intent.putExtra("herb_name", contacts.get(position).getHerbName());
+            intent.putExtra("latin_name", contacts.get(position).getLatinName());
+            intent.putExtra("temp", contacts.get(position).getTemp());
+            intent.putExtra("water", contacts.get(position).getWater());
+            intent.putExtra("humidity", contacts.get(position).getHumidity());
+            intent.putExtra("sun", contacts.get(position).getSun());
+            intent.putExtra("ph", contacts.get(position).getPh());
             startActivity(intent);
         };
     }
